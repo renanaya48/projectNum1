6fed71e15b705b64bfdd4628802b7f30c6409850
noaDagan <ndagan14@gmail.com>
2018-12-27 19:25:55 +0200

    proj1

---
+++ cmake-build-debug/CMakeFiles/projectNum1.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/noa/Desktop/C++/projectNum1/BindPathMap.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/BindPathMap.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/BoolExpression.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/BoolExpression.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/CheckEndScript.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/CheckEndScript.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/CheckForServerConnection.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/CheckForServerConnection.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/CommandMap.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/CommandMap.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/ConditionParser.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/ConditionParser.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/ConnectCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/ConnectCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/CreateCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/CreateCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/CreateExpression.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/CreateExpression.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Div.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Div.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/ExpressionCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/ExpressionCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/FlightValueMap.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/FlightValueMap.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/IfCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/IfCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/LoopCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/LoopCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Minus.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Minus.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Mul.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Mul.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Neg.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Neg.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Num.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Num.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/OpenServerCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/OpenServerCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Plus.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Plus.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/PrintCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/PrintCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/SetControlCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/SetControlCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/SleepCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/SleepCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/SymbolTable.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/SymbolTable.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/ValueControlForFlight.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/ValueControlForFlight.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/Var.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/Var.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/VarCommand.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/VarCommand.cpp.o"
+  "/home/noa/Desktop/C++/projectNum1/main.cpp" "/home/noa/Desktop/C++/projectNum1/cmake-build-debug/CMakeFiles/projectNum1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/projectNum1.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectNum1.dir/main.cpp.o"
+  "CMakeFiles/projectNum1.dir/Mul.cpp.o"
+  "CMakeFiles/projectNum1.dir/Minus.cpp.o"
+  "CMakeFiles/projectNum1.dir/Plus.cpp.o"
+  "CMakeFiles/projectNum1.dir/Div.cpp.o"
+  "CMakeFiles/projectNum1.dir/Num.cpp.o"
+  "CMakeFiles/projectNum1.dir/CreateCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/OpenServerCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/ConnectCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/CreateExpression.cpp.o"
+  "CMakeFiles/projectNum1.dir/LoopCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/ExpressionCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/Var.cpp.o"
+  "CMakeFiles/projectNum1.dir/ConditionParser.cpp.o"
+  "CMakeFiles/projectNum1.dir/IfCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/VarCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/SymbolTable.cpp.o"
+  "CMakeFiles/projectNum1.dir/PrintCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/SleepCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/BindPathMap.cpp.o"
+  "CMakeFiles/projectNum1.dir/FlightValueMap.cpp.o"
+  "CMakeFiles/projectNum1.dir/CheckForServerConnection.cpp.o"
+  "CMakeFiles/projectNum1.dir/ValueControlForFlight.cpp.o"
+  "CMakeFiles/projectNum1.dir/SetControlCommand.cpp.o"
+  "CMakeFiles/projectNum1.dir/Neg.cpp.o"
+  "CMakeFiles/projectNum1.dir/BoolExpression.cpp.o"
+  "CMakeFiles/projectNum1.dir/CommandMap.cpp.o"
+  "CMakeFiles/projectNum1.dir/CheckEndScript.cpp.o"
+  "projectNum1.pdb"
+  "projectNum1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectNum1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
